c64fde9b697d5df110ed7faa9c4c7103e20e2d22
oEdneiAresco <ednei.contato@gmail.com>
2026-08-13 16:54:29 -0300

    chore: setup solution architecture

---
+++ Sabemi.PaymentWebhook/tests/Sabemi.PaymentWebhook.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace Sabemi.PaymentWebhook.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
